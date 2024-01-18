c1e2073e0a61f7f1119fbcc3298d4350f5db3213
Lay <clarencetoledo08@gmail.com>
2024-01-18 19:11:10 +0800

    Added a new message

--- kubejs/client_scripts/Tooltips.js
+++ kubejs/client_scripts/Tooltips.js
@@ -1,6 +1,7 @@
 ItemEvents.tooltip(e => {
     e.add('avaritia:neutron_compressor', [Text.red("THE RECIPE FOR THIS IS DISABLED"), Text.red("Use the sequenced assembly recipe for the singularities")]);
-    e.add('avaritia:extreme_crafting_table', [Text.red("THE RECIPE FOR THIS IS DISABLED"), Text.red("Replaced with Mechanical Crafting")])
+    e.add('avaritia:extreme_crafting_table', [Text.red("THE RECIPE FOR THIS IS DISABLED"), Text.red("Replaced with Mechanical Crafting")]);
+    e.add('ars_nouveau:enchanters_sword', [Text.red("DO NOT GIVE THIS TO A GUARD VILLAGER", Text.red("OR IT WILL BE CLEARED ON A FIXED TIMER"))])
 });
 JEIEvents.removeRecipes(e => {
     e.remove('avaritia:compressing');
